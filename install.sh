3a1a417d91e5121ac9786c0f6d8336faeb64881e
Pedro Silva <pedromss91@gmail.com>
2018-09-15 10:37:21 +0100

    Adds conscript install instruction to global install script

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
 #! /usr/bin/env bash
 
 set -e
+logs_dir=logs
 
 source ./common.sh
+mkdir -p $logs_dir
 
 echo 'Performing symlinks...'
 
@@ -18,5 +20,9 @@ make_link "$dotfiles_fullpath/.local/share/nvim" $XDG_DATA_HOME/nvim
 echo 'Copying bin folder...'
 
 cp "$dotfiles_fullpath"/bin/* /usr/local/bin/
+
+echo 'Installing Conscript http://www.foundweekends.org/conscript ...'
+./conscript/install.sh > $logs_dir/conscript-install.log
+echo 'Finished installing conscript'
 
 echo 'All done!'
