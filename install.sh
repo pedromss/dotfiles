19db0e8c45faa982b40896ee3e19d7867669cf9d
Pedro Silva <pedromss91@gmail.com>
2019-06-23 19:36:34 +0100

    Adds git as a tool

--- install.sh
+++ install.sh
@@ -1,18 +1,18 @@
 #! /usr/bin/env bash
 
 set -e
+. runcom/.functions
 logs_dir=logs
 
 in_username="$USER"
-in_install_git='yes'
-in_install_vim='yes'
-in_install_nvim='yes'
-in_install_zsh='yes'
-in_install_zsh_plugins='yes'
-in_generate_brewfile='no'
-in_config_ctags='yes'
-in_install_fzf='yes'
-in_fzf_version='0.18.0'
+#in_install_git='yes'
+#in_install_vim='yes'
+#in_install_nvim='yes'
+#in_install_zsh='yes'
+#in_install_zsh_plugins='yes'
+#in_config_ctags='yes'
+#in_install_fzf='yes'
+#in_fzf_version='0.18.0'
 while [[ $# -gt 0 ]]
 do
   key="$1"
@@ -29,40 +29,24 @@ do
       in_install_rust=0
       shift
       ;;
-    --gen-brew)
-      in_generate_brewfile='yes'
-      shift
-      ;;
     --fzf-version)
       in_fzf_version=$2
       shift 2
       ;;
     --no-fzf)
-      in_install_fzf='no'
+      in_install_fzf=0
       shift
       ;;
     --no-zsh)
-      in_install_zsh='no'
+      in_install_zsh=0
       shift
       ;;
     --no-nvim)
-      in_install_nvim='no'
+      in_install_nvim=0
       shift
       ;;
     --no-zsh-plugins)
-      in_install_zsh_plugins='no'
-      shift
-      ;;
-    --no-git)
-      in_install_git='no'
-      shift
-      ;;
-    --no-vim)
-      in_install_vim='no'
-      shift
-      ;;
-    --no-ctags)
-      in_install_ctags='no'
+      in_install_zsh_plugins=0
       shift
       ;;
     *)
@@ -76,41 +60,20 @@ set -- "${POSITIONAL[@]}"
 source ./common.sh $@
 mkdir -p $logs_dir
 
-function clone_if_not_exists() {
-  if ! [ -d $2 ]; then
-    git clone --depth 1 $1 $2
-  fi
-}
-
-function generate_brewfile() {
-  if [[ "$1" == 'no' ]]; then
-    return
-  fi
-
-  echo 'Generating Brewfile'
-  curl -sL https://raw.githubusercontent.com/pedromss/brewfile-generator/master/gen-brewfile.sh | sh
+function clone-from-github () {
+  skip-if-dir-exists "$1" "$2"
+  git clone --depth 1 "https://github.com/$1" $2
 }
 
 function install_vim_plugins() {
-  if [[ "$1" == 'no' ]] || ! command_exists vim
-  then
-    return
-  fi
-
-  vim -c +PlugInstall +qall
+  require-tool 'vim'
+  vim -c +PlugInstall +qall 
 }
 
 function install_fzf() {
-  if [ -d "$HOME/.fzf" ] || [[ "$1" == 'no' ]]
-  then
-    return
-  fi
-
-  if ! command_exists git
-  then
-    echo 'Git is required to install fzf'
-    exit 1
-  fi
+  skip-if-requested 'fzf' $in_install_fzf
+  skip-if-dir-exists 'fzf' "$HOME/.fzf"
+  require-tool-to-install 'git' 'fzf'
 
   fzf_version="$2"
   git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
@@ -120,62 +83,37 @@ function install_fzf() {
 }
 
 function setup_neovim_config() {
-  if [[ "$1" == 'no' ]]
-  then
-    return
-  fi
+  skip-if-requested 'nvim' $in_install_nvim
   mkdir -p "$XDG_CONFIG_HOME/nvim"
   mkdir -p "$XDG_DATA_HOME/nvim"
 
+  # TODO probably shouldn't link these to dotfiles as they are specific to each host
   make_link "$dotfiles_fullpath/.local/share/nvim" $XDG_DATA_HOME/nvim
   make_link "$dotfiles_fullpath/.config/nvim/init.vim" "$HOME/.config/nvim/init.vim"
 }
 
 function install_zsh_plugins() {
-  if [[ "$1" == 'no' ]]
-  then
-    return
-  fi
+  skip-if-requested 'zsh-plugins' $in_install_zsh_plugins
 
-  mkdir -p $2
+  mkdir -p "$1"
 
-  clone_if_not_exists 'https://github.com/Cloudstek/zsh-plugin-appup.git' $zsh_plugins_folder/appup
-  clone_if_not_exists 'https://github.com/hlissner/zsh-autopair.git' $zsh_plugins_folder/zsh-autopair
-  clone_if_not_exists 'https://github.com/b4b4r07/enhancd.git' $zsh_plugins_folder/enhancd
-  clone_if_not_exists 'https://github.com/zdharma/fast-syntax-highlighting' $zsh_plugins_folder/fast-syntax-highlighting
-  clone_if_not_exists 'https://github.com/wfxr/forgit.git' $zsh_plugins_folder/forgit
-  clone_if_not_exists 'https://github.com/supercrabtree/k.git' $zsh_plugins_folder/k
-  clone_if_not_exists 'https://github.com/qoomon/zjump.git' $zsh_plugins_folder/zjump
-  clone_if_not_exists 'https://github.com/robbyrussell/oh-my-zsh.git' $zsh_plugins_folder/oh-my-zsh
-  clone_if_not_exists 'https://github.com/zsh-users/zsh-history-substring-search.git' $zsh_plugins_folder/zsh-history-substring-search
+  clone-from-github 'Cloudstek/zsh-plugin-appup.git' $zsh_plugins_folder/appup
+  clone-from-github 'hlissner/zsh-autopair.git' $zsh_plugins_folder/zsh-autopair
+  clone-from-github 'b4b4r07/enhancd.git' $zsh_plugins_folder/enhancd
+  clone-from-github 'zdharma/fast-syntax-highlighting' $zsh_plugins_folder/fast-syntax-highlighting
+  clone-from-github 'wfxr/forgit.git' $zsh_plugins_folder/forgit
+  clone-from-github 'supercrabtree/k.git' $zsh_plugins_folder/k
+  clone-from-github 'qoomon/zjump.git' $zsh_plugins_folder/zjump
+  clone-from-github 'robbyrussell/oh-my-zsh.git' $zsh_plugins_folder/oh-my-zsh
+  clone-from-github 'zsh-users/zsh-history-substring-search.git' $zsh_plugins_folder/zsh-history-substring-search
   if ! [ -f '/etc/os-release' ] || ! [[ $(cat /etc/os-release) =~ 'Raspbian' ]]; then
-    clone_if_not_exists 'https://github.com/zsh-users/zsh-autosuggestions.git' $zsh_plugins_folder/zsh-autosuggestions
+    clone-from-github 'zsh-users/zsh-autosuggestions.git' $zsh_plugins_folder/zsh-autosuggestions
   fi
 }
 
-function do_symlinks() {
-  if [[ "$1" == 'no' ]]
-  then
-    return
-  fi
-
-  shift
-
-  tolink=($@)
-  for link in "${tolink[@]}"
-  do
-    make_link "$dotfiles_fullpath/$link" "$user_home"
-  done
-}
 # ==================================================
 # Files to link
 # ==================================================
-misc_files_tolink[0]='runcom/.custom_profile'
-
-git_files_tolink[0]='tools/git/.gitconfig'
-
-bash_files_tolink[0]='runcom/.bash_profile'
-
 zsh_files_tolink[0]='zsh/.zshrc'
 zsh_files_tolink[1]='zsh/.zfunctions'
 
@@ -188,14 +126,8 @@ ctags_files_tolink[0]='tools/ctags/.ctags'
 # ==================================================
 # Make links
 # ==================================================
-do_symlinks 'dolink' "${misc_files_tolink[@]}"
-do_symlinks 'dolink' "${bash_files_tolink[@]}"
-
-do_symlinks "$in_install_git" "${git_files_tolink[@]}"
-do_symlinks "$in_install_zsh" "${zsh_files_tolink[@]}"
-do_symlinks "$in_install_vim" "${vim_files_tolink[@]}"
-do_symlinks "$in_install_ctags" "${ctags_files_tolink[@]}"
-
+create-link-at-home 'runcom/.custom_profile'
+create-link-at-home 'runcom/.bash_profile'
 # ==================================================
 # Tools
 # ==================================================
@@ -216,13 +148,13 @@ if (( ${in_install_golang:-1} )) ; then
     go \
     gomplate \
     vault
-fi
+  fi
 
-install_vim_plugins "$in_install_vim"
-setup_neovim_config "$in_install_nvim"
-install_fzf "$in_install_fzf" "$in_fzf_version"
-install_zsh_plugins "$in_install_zsh_plugins" "$zsh_plugins_folder"
-generate_brewfile "$in_generate_brewfile"
+  install_vim_plugins 
+  setup_neovim_config 
+  install_fzf "$in_fzf_version"
+  install_zsh_plugins "$zsh_plugins_folder"
+  generate_brewfile
 
 # ==================================================
 # Shutdown
