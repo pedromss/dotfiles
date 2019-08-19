05c3c0260d9caef5990263b1e73fb4632f93d19f
Pedro Silva <pedromss91@gmail.com>
2019-08-19 23:11:02 +0100

    Updates tmux

--- tools/tmux/install.sh
+++ tools/tmux/install.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
-. ../../funcs.sh
-
-if ! [ "$(command -v tmux)" ]; then
-  install-with-pkg-manager 'tmux'
-else
-  echo 'skipping: tmux is already installed!'
-fi
+. "$DOTFILES_FULL_PATH/funcs.sh"
 
 tmux_plugins_dir="$HOME/.tmux/plugins"
 tpm_dir="$tmux_plugins_dir/tpm"
+
+create-link-at-home 'tools/tmux/.tmux.conf'
+
+save-config 'DOTFILES_TMUX_TPM_DIR' "$tpm_dir"
+save-config 'DOTFILES_TMUX_PLUGINS_DIR' "$tmux_plugins_dir"
+
 echo "Installing tpm in ${tpm_dir}..."
 if ! [ -d "$tpm_dir" ]; then
   git clone --depth 1 https://github.com/tmux-plugins/tpm "$tpm_dir"
@@ -17,8 +17,8 @@ else
   echo "skipping: tpm already installed in $tpm_dir"
 fi
 
-create-link-at-home ".tmux.conf"
+skip-if-installed 'tmux'
+install-with-pkg-manager 'tmux'
 
-save-config 'DOTFILES_TMUX_TPM_DIR' "$tpm_dir"
-save-config 'DOTFILES_TMUX_PLUGINS_DIR' "$tmux_plugins_dir"
+
 
