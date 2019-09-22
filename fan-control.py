c694d6db76ba3215292ec2be63a41146fd5a6950
Pedro Silva <pedromss91@gmail.com>
2019-09-22 18:21:50 +0100

    Updates fan control script

--- fan-control.py
+++ fan-control.py
@@ -7,33 +7,33 @@ minspin = 10
 
 
 IO.setwarnings(False)
-  IO.setmode(IO.BCM)
-  IO.setup(14,IO.OUT)
-  fan = IO.PWM(14, 100)
+IO.setmode(IO.BCM)
+IO.setup(14,IO.OUT)
+fan = IO.PWM(14, 100)
 
-  cpu = CPUTemperature()
+cpu = CPUTemperature()
 
-  fan.start(0)
+fan.start(0)
 
-  oldtemp = cpu.temperature
+oldtemp = cpu.temperature
 
-  while True:
-      temp = cpu.temperature
-          if abs(temp - oldtemp) < 1.5:
-              time.sleep(1)
-          print(str(temp)+"  skipped")
-                  continue
-              oldtemp = temp
-                          if temp > 50:
-                              speed = ((temp - 45)*4)+minspin
-                                          if speed > 100:
-                                              speed = 100
-                                                              fan.ChangeDutyCycle(speed)
-          print(str(temp)+"  "+str(speed)+"  fan on")
-      else:
-          fan.ChangeDutyCycle(0)
-          print(str(temp)+"  fan off")
-              time.sleep(1)
-  fan.stop()
-  IO.cleanup()
-  print("done")
+while True:
+    temp = cpu.temperature
+    if abs(temp - oldtemp) < 1.5:
+        time.sleep(1)
+        print(str(temp)+"  skipped")
+        continue
+    oldtemp = temp
+    if temp > 50:
+        speed = ((temp - 45)*4)+minspin
+        if speed > 100:
+            speed = 100
+        fan.ChangeDutyCycle(speed)
+        print(str(temp)+"  "+str(speed)+"  fan on")
+    else:
+        fan.ChangeDutyCycle(0)
+        print(str(temp)+"  fan off")
+    time.sleep(1)
+fan.stop()
+IO.cleanup()
+print("done")
