7de5076d753f88e457e560b5df20f4e02476763a
Pedro Silva <pedromss91@gmail.com>
2020-02-05 17:50:53 +0000

    Updates coc settings. Fixes ag install on macos

--- tools/ag/install.sh
+++ tools/ag/install.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
 if ! (( "${DOTFILES_SHOULD_STOP_CURRENT:-0}" )) ; then
-  install-with-pkg-manager 'silversearcher-ag'
+  if is-macos ; then
+    install-with-pkg-manager 'the_silver_searcher'
+  else
+    install-with-pkg-manager 'silversearcher-ag'
+  fi
 fi
