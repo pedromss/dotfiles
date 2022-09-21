4e3ae88f8eca1c1145c66f74cc0104fceecf9e84
Pedro Silva <pedromss91@gmail.com>
2022-09-21 09:59:01 +0100

    feat(nvim): toggle folds on bufadd rather than bufenter

--- nvim/user/folds.lua
+++ nvim/user/folds.lua
@@ -4,20 +4,13 @@ function M.setup(configs)
 	local name = "fold_management"
 	augroup(name, { clear = true })
 	for _, c in ipairs(configs) do
-		cmd("BufEnter", {
+		cmd("BufAdd", {
 			desc = "Set foldmethod",
 			group = name,
 			pattern = c.pattern,
 			callback = function()
 				vim.wo.foldmethod = "indent"
 				vim.wo.wrap = true
-			end,
-		})
-		cmd("BufAdd", {
-			desc = "Set foldlevel",
-			group = name,
-			pattern = c.pattern,
-			callback = function()
 				vim.wo.foldlevel = c.fold_level
 			end,
 		})
