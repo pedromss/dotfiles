18d06ee535cdee035e5c44857809c9c4174dd8d4
Pedro Silva <pedromss91@gmail.com>
2024-08-09 19:25:51 +0100

    Adds elixir and python useful aliases

--- config.fish
+++ config.fish
@@ -28,9 +28,38 @@ abbr -a gsh git stash
 abbr -a gsp git stash pop
 abbr -a gp git push
 abbr -a gd git diff
+abbr -a swi git switch -
+abbr -a py python3
+abbr -a ipy ipython3
+abbr -a pip pip3
+abbr -a saws saml2aws exec --profile saml -- 
 
 set -gx EDITOR hx
 set -gx ERL_AFLAGS "-kernel shell_history enabled"
 fish_add_path --path /usr/local/go/bin
 fish_add_path --path $HOME/go/bin
 source_if_present $HOME/.tmonly.fish
+
+# Elixir things
+abbr -a mdp mix deps.get
+abbr -a mc mix compile
+abbr -a mtw mix test.watch
+abbr -a mcc mix credo
+abbr -a mf mix format
+abbr -a iex iex -S mix run --no-start
+abbr -a phx iex -S mix phx.server
+abbr -a md mix dialyzer
+abbr -a mdf mix dialyzer --format ignore_file_strict
+abbr -a tenv MIX_ENV=test 
+abbr -a lgi LOGGER_LEVEL=info
+abbr -a lgd LOGGER_LEVEL=debug
+abbr -a lgw LOGGER_LEVEL=warn
+
+
+# BEGIN opam configuration
+# This is useful if you're using opam as it adds:
+#   - the correct directories to the PATH
+#   - auto-completion for the opam binary
+# This section can be safely removed at any time if needed.
+test -r '/Users/psilva/.opam/opam-init/init.fish' && source '/Users/psilva/.opam/opam-init/init.fish' > /dev/null 2> /dev/null; or true
+# END opam configuration
